e6f927e4d68b1f495dca8172c0d931ce5b1d0e7a
Gauthier_3090 <gauthier.pladet@gmail.com>
2022-02-24 15:19:31 +0100

    Install Node.JS and test pixiJS

--- js/game.js
+++ js/game.js
@@ -1,11 +1,14 @@
-const db = require('mongoose')
+// Create the application helper and add its render target to the page
+let app = new PIXI.Application({ width: 640, height: 360 });
+document.body.appendChild(app.view);
 
-main().catch(err => console.log(err));
+// Create the sprite and add it to the stage
+let sprite = PIXI.Sprite.from('images/character.png');
+app.stage.addChild(sprite);
 
-async function main() {
-    await db.connect('mongodb://localhost:27017/test');
-}
-
-let app = new PIXI.Application({width: 840, height: 640})
-
-document.body.appendChild(app.view)
+// Add a ticker callback to move the sprite back and forth
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+    elapsed += delta;
+    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
