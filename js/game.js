e818663db752c9e00c39e388ad193321be1c9e94
Gauthier_3090 <gauthier.pladet@gmail.com>
2022-02-24 15:56:57 +0100

    Creation Menu

--- js/game.js
+++ js/game.js
@@ -1,14 +1,22 @@
-// Create the application helper and add its render target to the page
-let app = new PIXI.Application({ width: 640, height: 360 });
-document.body.appendChild(app.view);
+const WIDTH = 920
+const HEIGHT = 600
 
-// Create the sprite and add it to the stage
-let sprite = PIXI.Sprite.from('images/character.png');
-app.stage.addChild(sprite);
+function initTitle(text, app, x, y) {
+    let title = new PIXI.Text(text)
+    title.x = x
+    title.y = y
+    title.style.fill = '#FFF'
+    app.stage.addChild(title)
+}
 
-// Add a ticker callback to move the sprite back and forth
-let elapsed = 0.0;
-app.ticker.add((delta) => {
-    elapsed += delta;
-    sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
-});
+function initApp() {
+    return new PIXI.Application({ width: WIDTH, height: HEIGHT });
+}
+
+function run() {
+    let app = initApp()
+    document.body.appendChild(app.view);
+    initTitle('Lovers Run', app, 400, 0)
+}
+
+run()
